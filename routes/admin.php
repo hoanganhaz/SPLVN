13a788716559f53981aa5574ad04c0edb8db9eb0
hoanganhaz <anhnhph38484@fpt.edu.vn>
2025-12-16 20:19:34 +0700

    update

---
+++ routes/admin.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\Admin\HomeController;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::prefix('admin')
+    ->group(function () {
+        Route::get('/', [HomeController::class, 'index'])->name('admin.home');
+    });
+
+ 
